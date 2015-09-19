2c55fb822c775e4276a55f6447404fe213aef61a
lutfirm <maghfirohlutfi@gmail.com>
2015-09-19 17:36:32 +0700

    Ini adalah Socket Programming tugas untuk tanggal 22 September 2015. SocketProgrammingClient untuk program clientnya. SocketProgrammingServer untuk program clientnya

---
+++ SocketProgrammingServer/build/classes/socketprogrammingserver/Family.sql
@@ -0,0 +1,8 @@
+SELECT a_few_moments
+FROM your_busy_life
+INTO your_loved_ones_life
+FOR ALL possible_occassions
+WHERE your_spare_time IS NULL
+OR office_time > LARGE
+OR work_pressure IN ('much','too much')
+GROUP BY your_family
